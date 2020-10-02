54090f79d9cd008e6acce04ffa2822d9656c5bec
JP <jpjones22@gmail.com>
2020-10-02 16:12:48 -0700

    Completed sumArray challenge

--- app.js
+++ app.js
@@ -77,14 +77,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
-
+//sumArr
 function sumArray(sumArr) { //eslint-disable-line
+  var sumElementsArray = sum(sumArr[0], sumArr[1], sumArr[2])[0];
+  var stringE2 = sumArr[0] + ',' + sumArr[1] + ',' + sumArr[2] + ' was passed in as an array of numbers, and ' + sumElementsArray + ' is their sum.';
 
+  return [sumElementsArray, stringE2];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
